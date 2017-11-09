e28674e89a81238d073a35445cd74a050bd3df9a
Morgan Mendis <morganandrewmendis@gmail.com>
2017-11-08 20:47:37 -0500

    Created initial mock HTML for network display

--- app_hello.py
+++ app_hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, g
+from flask import Flask, g,render_template
 import sqlite3
 import os
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "hello"
+    return render_template('network_display.html')
 
 @app.route('/bye')
 def bye():
