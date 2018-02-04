ccf2c50bb3664c98f79a34a7b001121269c342f6
haxwithaxe <me@haxwithaxe.net>
2018-02-04 16:30:49 -0500

    using sqlalchemy stuff instead of my wacky crap

--- wifi_occupancy_sensor/__init__.py
+++ wifi_occupancy_sensor/__init__.py
@@ -3,30 +3,19 @@ import os
 
 from flask import Flask, g
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
-from wifi_occupancy_sensor.controllers import DB, Devices, Users
+from wifi_occupancy_sensor.controllers import database
+from wifi_occupancy_sensor.models import Devices, User
+
 
 app = Flask(__name__)
 app.config.from_pyfile(os.environ['WIFI_OCCUPANCY_SENSOR_CONFIGFILE'])
 api = Api(app)
-
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = DB(app.config, Devices, Users)
-    return db
-
-
-def devices():
-    return Devices(get_db())
-
-
-def users():
-    return Users(get_db())
-
+db = SQLAlchemy(app, model_class=database.Model)
+users = database.Helper(db.session, User)
+devices = Devices(db.session)
 
 from wifi_occupancy_sensor.views import rest
 api.add_resource(rest.Users, '/users')
 api.add_resource(rest.Devices, '/devices')
-
